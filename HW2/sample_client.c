39c9073942d4c439c5efb6678d583cb66432e3ac
Johnu George <johnugeorge109@gmail.com>
2013-02-01 21:08:13 -0500

    Reverting back to uint8 and uint32 types.compiles

--- HW2/sample_client.c
+++ HW2/sample_client.c
@@ -5,14 +5,14 @@ int main(int argc, char** argv)
 {
 	lsp_client* myclient = lsp_client_create("127.0.0.1", atoi(argv[1]));
 	
-	char message[] = "ilovethiscoursealready";
+	const char message[] = "ilovethiscoursealready";
 
-	lsp_client_write(myclient, (char *) message, strlen(message));
+	lsp_client_write(myclient, (uint8_t*) message, strlen(message));
 	
 	uint8_t buffer[4096];
 	int bytes_read = lsp_client_read(myclient, buffer);
 	
-	puts(buffer);
+	puts((const char*)buffer);
 	
 	lsp_client_close(myclient);
 	
